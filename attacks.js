858c178612fa79233529d5ce157a9f1b11e5af5f
xlelord9292 <140418730+xlelord9292@users.noreply.github.com>
2024-06-24 15:33:38 -0400

    Update attacks.js

--- attacks.js
+++ attacks.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
-const { execSync } = require('child_process');
 const fs = require('fs');
 const net = require('net');
 const dgram = require('dgram');
 const http = require('http');
-const url = require('url');
-
 
 const MAX_CONNECTIONS_PER_IP = 100;
 const CONNECTION_TIMEOUT = 60000;
@@ -17,6 +14,7 @@ const LOG_FILE = 'logs.txt';
 const connections = {};
 const blacklist = {};
 const requestCounts = {};
+const loggedIPs = new Set();
 
 function addToBlacklist(ip) {
     console.log(`Blacklisting IP address: ${ip}`);
@@ -24,19 +22,22 @@ function addToBlacklist(ip) {
     setTimeout(() => {
         console.log(`Removing IP address from blacklist: ${ip}`);
         delete blacklist[ip];
+        loggedIPs.delete(ip);  // Remove IP from logged set when removed from blacklist
     }, BLACKLIST_TIMEOUT);
 }
 
-function logDDoSAttack(ip, pps) {
-    const logLine = `DDoS Attack Detected: IP ${ip} | Packets per second: ${pps}\n`;
+function logDDoSAttack(ip, pps, port) {
+    const logLine = `DDoS Attack Detected: IP ${ip} | Packets per second: ${pps} | Port: ${port}\n`;
     fs.appendFile(LOG_FILE, logLine, (err) => {
         if (err) {
             console.error('Error writing to logs file:', err);
+        } else {
+            loggedIPs.add(ip);
         }
     });
 }
 
-function handleIncomingData(socket, remoteAddress) {
+function handleIncomingData(socket, remoteAddress, remotePort) {
     let packetCount = 0;
     let pps = 0;
     const interval = setInterval(() => {
@@ -46,34 +47,36 @@ function handleIncomingData(socket, remoteAddress) {
 
     socket.on('data', data => {
         packetCount++;
-        console.log(`Received data from ${remoteAddress}: ${data}`);
+        console.log(`Received data from ${remoteAddress}:${remotePort} - ${data}`);
     });
 
     socket.on('end', () => {
         clearInterval(interval);
         connections[remoteAddress]--;
-        console.log(`Connection closed with ${remoteAddress}`);
+        console.log(`Connection closed with ${remoteAddress}:${remotePort}`);
     });
 
     socket.on('error', err => {
         clearInterval(interval);
-        console.error(`Error with connection from ${remoteAddress}: ${err.message}`);
+        console.error(`Error with connection from ${remoteAddress}:${remotePort} - ${err.message}`);
         connections[remoteAddress]--;
         socket.destroy();
     });
 
     setInterval(() => {
         if (pps > MAX_CONNECTIONS_PER_IP) {
-            console.log(`DDoS attack detected from ${remoteAddress}. Packets per second: ${pps}`);
-            logDDoSAttack(remoteAddress, pps);
+            console.log(`DDoS attack detected from ${remoteAddress}:${remotePort}. Packets per second: ${pps}`);
+            if (!loggedIPs.has(remoteAddress)) {
+                logDDoSAttack(remoteAddress, pps, remotePort);
+            }
             addToBlacklist(remoteAddress);
-            clearInterval(interval); 
-            socket.destroy(); 
+            clearInterval(interval);
+            socket.destroy();
         }
     }, 1000);
 }
 
-function applyFirewallRules(socket, remoteAddress) {
+function applyFirewallRules(socket, remoteAddress, remotePort) {
     if (blacklist[remoteAddress]) {
         console.log(`Rejected connection from blacklisted IP: ${remoteAddress}`);
         socket.destroy();
@@ -82,43 +85,66 @@ function applyFirewallRules(socket, remoteAddress) {
     connections[remoteAddress] = (connections[remoteAddress] || 0) + 1;
     socket.setTimeout(CONNECTION_TIMEOUT, () => {
         connections[remoteAddress]--;
-        console.log(`Connection timeout for ${remoteAddress}`);
+        console.log(`Connection timeout for ${remoteAddress}:${remotePort}`);
     });
-    handleIncomingData(socket, remoteAddress);
+    handleIncomingData(socket, remoteAddress, remotePort);
 }
 
 const tcpServer = net.createServer(socket => {
-    const { remoteAddress } = socket;
-    console.log(`Incoming TCP connection from ${remoteAddress}`);
-    applyFirewallRules(socket, remoteAddress);
+    const remoteAddress = socket.remoteAddress;
+    const remotePort = socket.remotePort;
+    console.log(`Incoming TCP connection from ${remoteAddress}:${remotePort}`);
+    applyFirewallRules(socket, remoteAddress, remotePort);
 });
-
 
 const udpServer = dgram.createSocket('udp4');
 udpServer.on('error', (err) => {
     console.error(`UDP server error:\n${err.stack}`);
     udpServer.close();
 });
+
+const udpConnections = {};
+
 udpServer.on('message', (msg, rinfo) => {
     const remoteAddress = rinfo.address;
-    console.log(`Incoming UDP message from ${remoteAddress}`);
-    applyFirewallRules(udpServer, remoteAddress);
+    const remotePort = rinfo.port;
+    console.log(`Incoming UDP message from ${remoteAddress}:${remotePort}`);
+    if (!udpConnections[remoteAddress]) {
+        udpConnections[remoteAddress] = { count: 0, interval: null };
+        udpConnections[remoteAddress].interval = setInterval(() => {
+            const pps = udpConnections[remoteAddress].count;
+            udpConnections[remoteAddress].count = 0;
+            if (pps > MAX_CONNECTIONS_PER_IP) {
+                console.log(`DDoS attack detected from ${remoteAddress}:${remotePort}. Packets per second: ${pps}`);
+                if (!loggedIPs.has(remoteAddress)) {
+                    logDDoSAttack(remoteAddress, pps, remotePort);
+                }
+                addToBlacklist(remoteAddress);
+                clearInterval(udpConnections[remoteAddress].interval);
+                delete udpConnections[remoteAddress];
+            }
+        }, 1000);
+    }
+    udpConnections[remoteAddress].count++;
+    if (blacklist[remoteAddress]) {
+        console.log(`Rejected UDP message from blacklisted IP: ${remoteAddress}`);
+        return;
+    }
 });
+
 udpServer.on('listening', () => {
     const address = udpServer.address();
     console.log(`UDP server listening ${address.address}:${address.port}`);
 });
-udpServer.bind();
+udpServer.bind(0);  // Bind to a random available port
 
-const PORT = 0; 
-tcpServer.listen(PORT, () => {
-    console.log(`TCP server is listening on all available ports`);
+const tcpPort = 5775;
+tcpServer.listen(tcpPort, () => {
+    console.log(`TCP server is listening on port ${tcpPort}`);
 });
-
 
 const app = express();
 const proxy = httpProxy.createProxyServer({});
-
 
 app.use((req, res, next) => {
     const remoteAddress = req.connection.remoteAddress;
@@ -135,7 +161,9 @@ app.use((req, res, next) => {
 
     if (requestCount > MAX_REQUESTS_PER_MINUTE) {
         console.log(`DDoS attack detected from ${remoteAddress}. Requests per minute: ${requestCount}`);
-        logDDoSAttack(remoteAddress, requestCount);
+        if (!loggedIPs.has(remoteAddress)) {
+            logDDoSAttack(remoteAddress, requestCount, 'HTTP');
+        }
         addToBlacklist(remoteAddress);
         res.status(403).send('Forbidden');
     } else {
@@ -143,14 +171,23 @@ app.use((req, res, next) => {
     }
 });
 
-
-
-
 app.use((req, res) => {
     const target = 'http://localhost';
     proxy.web(req, res, { target: `${target}${req.url}` });
 });
 
-app.listen(5587, () => {
-    console.log('HTTP server with Layer 7 protection is listening on port 5587');
+// Redirect heavy traffic to another server
+app.use((req, res, next) => {
+    const remoteAddress = req.connection.remoteAddress;
+    if (connections[remoteAddress] > MAX_CONNECTIONS_PER_IP) {
+        console.log(`Redirecting heavy traffic from ${remoteAddress}`);
+        res.redirect('http://109.71.253.231');
+    } else {
+        next();
+    }
+});
+
+const httpPort = 8080;
+app.listen(httpPort, () => {
+    console.log(`HTTP server is listening on port ${httpPort}`);
 });
